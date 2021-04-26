67b22f69040d4af66269336318707b7b4ba97144
Chloe Hellberg <chloe.hellberg@gmail.com>
2021-04-26 15:12:39 -0700

    Clean errors.

--- src/journal.js
+++ src/journal.js
@@ -49,24 +49,28 @@ export class Plant {
     if (this.type == "Succulent") {
       daysTilWatering += 10;
       this.waterDay = daysTilWatering - inputDate;
-      this.waterNote = "You will need to water your plant about every 10 days. In addition, if the leaves feel rubbery and bend easily at any point, please water your succulent. If the leaves change color, you are over watering your succulent.";
+      this.waterNote =
+        "You will need to water your plant about every 10 days. In addition, if the leaves feel rubbery and bend easily at any point, please water your succulent. If the leaves change color, you are over watering your succulent.";
     } else if (this.type == "Cactus") {
       daysTilWatering += 14;
       this.waterDay = daysTilWatering - inputDate;
-      this.waterNote = "You will need to water your plant about every 14 days. Let cacti dry completely between waterings.";
+      this.waterNote =
+        "You will need to water your plant about every 14 days. Let cacti dry completely between waterings.";
     } else if (this.type == "Flower") {
       daysTilWatering += 7;
       this.waterDay = daysTilWatering - inputDate;
-      this.waterNote = "Many indoor flowers (except orchids) will need to be watered every 7 days.";
+      this.waterNote =
+        "Many indoor flowers (except orchids) will need to be watered every 7 days.";
     } else {
       daysTilWatering += 4;
       this.waterDay = daysTilWatering - inputDate;
-      this.waterNote = "You will need to water your plant about every 4 days. Ferns like to be misted, and moist but not waterlogged.";
+      this.waterNote =
+        "You will need to water your plant about every 4 days. Ferns like to be misted, and moist but not waterlogged.";
     }
     if (this.waterDay > 0) {
       this.waterDay += " days until the next watering";
     } else {
-      this.waterDay = "Water Me Please!"
+      this.waterDay = "Water Me Please!";
     }
   }
   resetWaterDay() {
@@ -80,15 +84,15 @@ export class Plant {
       this.waterDay = "4 days until the next watering";
     }
   }
-  createTypeSpan() {
-    if (this.type == "Succulent") {
-      let succulentSpan = "<span>10 days until the next watering</span>";
-    } else if (this.type == "Cactus") {
-      let cactusSpan = "<span>14 days until the next watering</span>";
-    } else if (this.type == "Flower") {
-      let flowerSpan = "<span>7 days until the next watering</span>";
-    } else {
-      let fernSpan = "<span>4 days until the next watering</span>";
-    }
-  }
+  // createTypeSpan() {
+  //   if (this.type == "Succulent") {
+  //     let succulentSpan = "<span>10 days until the next watering</span>";
+  //   } else if (this.type == "Cactus") {
+  //     let cactusSpan = "<span>14 days until the next watering</span>";
+  //   } else if (this.type == "Flower") {
+  //     let flowerSpan = "<span>7 days until the next watering</span>";
+  //   } else {
+  //     let fernSpan = "<span>4 days until the next watering</span>";
+  //   }
+  // }
 }
